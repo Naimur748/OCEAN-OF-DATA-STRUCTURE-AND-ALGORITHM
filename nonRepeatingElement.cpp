8bae347e524cc16f4259707fe33601d7c87d117c
Naimur Rahman <naimur748@gmail.com>
2024-11-15 19:48:18 +0600

    It is Not repeating element

---
+++ nonRepeatingElement.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int firstNonRepeatingElement(vector<int>& arr){
+
+    unordered_map<int, int> freq;
+
+    for(int num : arr){
+        freq[num]++
+    }
+
+    for(int num : arr){
+        if(freq[num] == 1){
+            return num;
+        }
+    }
+    return -1;
+}
+
+
+int main(){
+
+    vector <int> arr = {5, 3, 4, 3, 5, 6, 4};\
+
+    int result = firstNonRepeatingElement(arr);
+
+    if(result != -1){
+        cout<<"First non-repeating Element"<<result <<endl;
+    }else{
+        cout<<"Not found Non repeating Element"<<endl;
+    }
+
+
+}
