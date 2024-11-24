ad2049f0d5f0bd6836c3de4cb6f1f2b24e02247f
Naimur Rahman <naimur748@gmail.com>
2024-11-24 14:19:44 +0600

    How can we find duplicate element from an array

---
+++ ProblemSolving/FindDuplicateElement.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+void removeDuplicteElement(int arr[], int size){
+
+    unordered_set<int> seen;
+
+    for(int i=0; i<size; i++){
+        if(seen.find(arr[i]) != seen.end()){
+            cout<<arr[i]<<" ";
+        }else{
+            seen.insert(arr[i]);
+        }
+    }
+
+}
+
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 2, 5, 6,3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    removeDuplicteElement(arr, size);
+   
+   
+    
+}
