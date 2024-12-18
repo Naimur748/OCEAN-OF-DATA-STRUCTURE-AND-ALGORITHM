bae43bac5cd429c35f78f3400c57ada5affd3429
Naimmur748 <naimur748@gmail.com>
2024-12-18 23:22:34 +0600

    Link List practice code

---
+++ LinkListQuistion/PureLinkList.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+void append(Node*& head, int value){
+
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = nullptr;
+
+    if(head == nullptr){
+        head = newNode;
+    }else{
+        Node* temp = head;
+        while(temp->next!= nullptr){
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+
+}
+
+void printList(Node* head){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL";
+}
+
+int main(){
+
+    Node* head = nullptr;
+
+    append(head, 10);
+    append(head, 20);
+    append(head, 30);
+
+    printList(head);
+
+    return 0;
+}
