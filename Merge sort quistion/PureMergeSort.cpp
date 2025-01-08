836dbbdd584e4b84dcd549f6cc0d69a7db088455
Naimmur748 <naimur748@gmail.com>
2025-01-08 21:54:42 +0600

    Merge sort Explanation

---
+++ Merge sort quistion/PureMergeSort.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+
+//https://chatgpt.com/c/677e825a-cbb8-800e-8267-e70d01095b35
+
+void merge(int arr[], int left, int mid, int right){
+
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+
+    for(int i=0; i<n1; i++)
+       L[i] = arr[left + i];
+
+    for(int j=0; j<n2; j++)
+        R[j] = arr[mid + 1 + j];
+    
+  //Merge process
+  int i = 0, j = 0, k = left;
+
+  while(i < n1 && j < n2){
+
+    if(L[i] <= R[j]){
+
+        arr[k] = L[i];
+
+        i++;
+
+    }else{
+
+        arr[k] = R[j];
+        j++;
+
+    }
+    k++;
+  }
+
+
+  while(i < n1){
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while(j < n2){
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
+
+    delete[] L;
+    delete[] R;
+
+}
+
+void mergeSort(int arr[], int left, int right){
+    
+    if(left < right){
+        int mid = left + (right-left) / 2;
+
+        mergeSort(arr, left, mid);
+
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+
+    }
+}
+
+int main(){
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout<<"Orginal array: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<endl;
+
+    mergeSort(arr, 0, n-1);
+
+
+    cout<<"Sorted array: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+
+
+}
