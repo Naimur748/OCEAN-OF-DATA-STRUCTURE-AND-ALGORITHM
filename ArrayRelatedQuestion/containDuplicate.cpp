486ef3ed36eda7ec54a878a7b9260ce06909d8a6
Naimmur748 <naimur748@gmail.com>
2025-08-01 21:15:05 +0600

    This is Revision the code

--- ArrayRelatedQuestion/containDuplicate.cpp
+++ ArrayRelatedQuestion/containDuplicate.cpp
@@ -1,25 +1,62 @@
+// #include <iostream>
+// #include <unordered_set>
+// using namespace std;
+
+// int main(){
+
+//     int arr[] = {2, 4, 6, 1, 2, 7};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     unordered_set<int> seen;
+
+//     bool isduplicate = false;
+
+//     for(int i=0; i<n; i++){
+//         if(seen.count(arr[i])){
+//             isduplicate = true;
+//             break;
+//         }
+//         seen.insert(arr[i]);   
+//     }
+
+//     if(isduplicate){
+
+//         cout<<"True"<<endl;
+
+//     }else{
+
+//         cout<<"False"<<endl;
+
+//     }
+    
+//     return 0;
+// }
+
 #include <iostream>
 #include <unordered_set>
 using namespace std;
 
+
 int main(){
 
-    int arr[] = {2, 4, 6, 1, 2, 7};
+    int arr[] = {4, 3, 1, 6, 7, 8, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     unordered_set<int> seen;
 
-    bool isduplicate = false;
+    bool isDuplicate = false;
 
     for(int i=0; i<n; i++){
+
         if(seen.count(arr[i])){
-            isduplicate = true;
-            break;
+
+            isDuplicate = true;
         }
-        seen.insert(arr[i]);   
+
+        seen.insert(arr[i]);
     }
 
-    if(isduplicate){
+    if(isDuplicate){
 
         cout<<"True"<<endl;
 
@@ -28,8 +65,11 @@ int main(){
         cout<<"False"<<endl;
 
     }
-    
-    return 0;
 }
+
+
+
+
+
 
 
