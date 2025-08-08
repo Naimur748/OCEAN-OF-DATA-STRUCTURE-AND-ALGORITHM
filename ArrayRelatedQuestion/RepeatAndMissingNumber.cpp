043f1529ea208ac94fb5125daea76bb38d422f2e
Naimmur748 <naimur748@gmail.com>
2025-08-08 07:28:37 +0600

    this problem solve another time in same file

--- ArrayRelatedQuestion/RepeatAndMissingNumber.cpp
+++ ArrayRelatedQuestion/RepeatAndMissingNumber.cpp
@@ -2,18 +2,50 @@
 #include <vector>
 using namespace std;
 
+// vector<int> findRepeatAndMissing(const vector<int>& nums){
+
+//     long long n = nums.size();
+
+//     long long expectedSum = n * (n + 1) / 2;
+//     long long expectedSquareSum = n * (n+1) * (2 * n+1) / 6;
+
+//     long long actualSum = 0, actualSquareSum = 0;
+
+//     for(int num : nums){
+//         actualSum += (long long)num;
+//         actualSquareSum += (long long)num * (long long)num;
+//     }
+
+//     long long diff1 = actualSum - expectedSum;
+//     long long diff2 = actualSquareSum - expectedSquareSum;
+
+//     long long sumAB = diff2 / diff1;
+
+//     long long A = (diff1 + sumAB) / 2;
+//     long long B =  A - diff1;
+
+//     return {(int)A , (int)B};
+
+// }
+
+
+
+
+
+
 vector<int> findRepeatAndMissing(const vector<int>& nums){
 
     long long n = nums.size();
 
-    long long expectedSum = n * (n + 1) / 2;
+    long long expectedSum = n * (n+1) / 2;
     long long expectedSquareSum = n * (n+1) * (2 * n+1) / 6;
 
     long long actualSum = 0, actualSquareSum = 0;
-
     for(int num : nums){
-        actualSum += (long long)num;
-        actualSquareSum += (long long)num * (long long)num;
+
+        actualSum += (long long) num;
+        actualSquareSum += (long long )num * (long long) num;
+
     }
 
     long long diff1 = actualSum - expectedSum;
@@ -21,12 +53,14 @@ vector<int> findRepeatAndMissing(const vector<int>& nums){
 
     long long sumAB = diff2 / diff1;
 
-    long long A = (diff1 + sumAB) / 2;
-    long long B =  A - diff1;
+    long long A = (diff1 + sumAB) / 2 ;
 
-    return {(int)A , (int)B};
+    long long B = A - diff1;
+
+    return {(int)A, (int)B};
 
 }
+
 
 int main(){
     vector<int> nums = {3, 1, 2, 5, 3};
@@ -35,3 +69,5 @@ int main(){
     cout << "Repeated: " << result[0] << ", Missing: " << result[1] << endl;
     return 0;
 }
+
+
