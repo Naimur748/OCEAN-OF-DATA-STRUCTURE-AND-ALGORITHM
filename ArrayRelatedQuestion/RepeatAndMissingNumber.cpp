8cc60238ebd6a9131c050dfd086dd72a1ab8920e
Naimmur748 <naimur748@gmail.com>
2025-08-08 17:50:39 +0600

    This problem solution will be use Math mathics

--- ArrayRelatedQuestion/RepeatAndMissingNumber.cpp
+++ ArrayRelatedQuestion/RepeatAndMissingNumber.cpp
@@ -33,19 +33,46 @@ using namespace std;
 
 
 
-vector<int> findRepeatAndMissing(const vector<int>& nums){
+// vector<int> findRepeatAndMissing(const vector<int>& nums){
+
+//     long long n = nums.size();
+
+//     long long expectedSum = n * (n+1) / 2;
+//     long long expectedSquareSum = n * (n+1) * (2 * n+1) / 6;
+
+//     long long actualSum = 0, actualSquareSum = 0;
+//     for(int num : nums){
+
+//         actualSum += (long long) num;
+//         actualSquareSum += (long long )num * (long long) num;
+
+//     }
+
+//     long long diff1 = actualSum - expectedSum;
+//     long long diff2 = actualSquareSum - expectedSquareSum;
+
+//     long long sumAB = diff2 / diff1;
+
+//     long long A = (diff1 + sumAB) / 2 ;
+
+//     long long B = A - diff1;
+
+//     return {(int)A, (int)B};
+
+// }
+
+vector<int> findRepeatAndMissing(vector<int>& nums){
 
     long long n = nums.size();
 
     long long expectedSum = n * (n+1) / 2;
-    long long expectedSquareSum = n * (n+1) * (2 * n+1) / 6;
+    long long expectedSquareSum = n * (n+1) * (2*n+2) / 6;
 
     long long actualSum = 0, actualSquareSum = 0;
+
     for(int num : nums){
-
         actualSum += (long long) num;
-        actualSquareSum += (long long )num * (long long) num;
-
+        actualSquareSum += (long long) num  * (long long)num;
     }
 
     long long diff1 = actualSum - expectedSum;
@@ -53,12 +80,10 @@ vector<int> findRepeatAndMissing(const vector<int>& nums){
 
     long long sumAB = diff2 / diff1;
 
-    long long A = (diff1 + sumAB) / 2 ;
-
+    long long A = (diff1 + sumAB) / 2;
     long long B = A - diff1;
 
     return {(int)A, (int)B};
-
 }
 
 
