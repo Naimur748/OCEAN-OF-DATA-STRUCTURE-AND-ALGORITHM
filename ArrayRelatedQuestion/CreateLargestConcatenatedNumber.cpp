a72673a6a4acc6e59d3dcdd76b7836e72f57df37
Naimmur748 <naimur748@gmail.com>
2025-08-21 00:49:54 +0600

    Create largest concatenated number

---
+++ ArrayRelatedQuestion/CreateLargestConcatenatedNumber.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+bool compareForLargestNumber(const string &plateA, const string &plateB){
+
+    return plateA + plateB > plateB + plateA;
+}
+
+
+string createLargestDisplayNumber(vector<int>& carNumber){
+
+    vector<string> numberPlates;
+    numberPlates.reserve(carNumber.size());
+
+    for(int number : carNumber){
+        numberPlates.push_back(to_string(number));
+    }
+
+    sort(numberPlates.begin(), numberPlates.end(), compareForLargestNumber);
+
+    if(numberPlates[0] == "0") return "0";
+
+    string largestDisplayNumber;
+    for(const string &plate : numberPlates){
+        largestDisplayNumber += plate;
+    }
+
+    return largestDisplayNumber;
+
+}
+
+int main(){
+
+    vector<int> carNumbers = {54, 546, 548, 60};
+
+    string vipDisplay = createLargestDisplayNumber(carNumbers);
+
+    cout << "VIP Display Number: " << vipDisplay << endl;
+    // Output হবে: 6054854654
+
+    return 0;
+
+}
