273cf6ab9f74659740add27d54310c961080ba43
Naimmur748 <naimur748@gmail.com>
2025-08-29 05:56:27 +0600

    After another day permanently solve this problem

--- ArrayRelatedQuestion/ThreeSum.cpp
+++ ArrayRelatedQuestion/ThreeSum.cpp
@@ -3,6 +3,12 @@
 #include <algorithm>
 using namespace std;
 
+
+/*
+
+
+
+
 vector<vector<int>> findRiskNeutralPortfolios(vector<int>& profitLossList){
     
     vector<vector<int>> riskFreeCombos;
@@ -63,8 +69,6 @@ void printPortfolios(const vector<vector<int>>& portfolios) {
 
 
 
-
-
 int main(){
     vector<int> profitLossList = {-200, 100, 100, 50, -50, 0};
 
@@ -77,4 +81,71 @@ int main(){
     return 0;
 
 
+}
+
+*/
+
+
+class Solution{
+    public:
+
+    vector<vector<int>> threeSum(vector<int>& nums){
+        vector<vector<int>> result;
+
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for(int i=0; i<n-2; i++){
+
+            if(i>0 && nums[i] == nums[i-1]) continue;
+
+            int left = i + 1;
+            int right = n-1;
+
+            while(left < right){
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0){
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right --;
+
+                    left++;
+                    right--;
+
+                }else if(sum < 0){
+
+                    left++;
+
+                }else{
+
+                    right--;
+                }
+
+            }
+        }
+
+        return result;
+
+    }
+
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {-1,0,1,2,-1,-4};
+    vector<vector<int>> ans = sol.threeSum(nums);
+
+    cout << "Output: \n";
+    for(auto triplet : ans) {
+        cout << "[";
+        for(int i = 0; i < triplet.size(); i++) {
+            cout << triplet[i];
+            if(i < triplet.size()-1) cout << ",";
+        }
+        cout << "]\n";
+    }
+    return 0;
 }
