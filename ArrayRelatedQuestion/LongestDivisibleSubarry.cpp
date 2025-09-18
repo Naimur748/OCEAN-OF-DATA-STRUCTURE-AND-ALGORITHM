55c65746efff185f09ddc247c1135eab081718d5
Md. Naimur Rahman <naimur748@gmail.com>
2025-09-18 07:38:18 +0600

    Updated LongestDivisibleSubarry.cpp - added comment

--- ArrayRelatedQuestion/LongestDivisibleSubarry.cpp
+++ ArrayRelatedQuestion/LongestDivisibleSubarry.cpp
@@ -4,30 +4,62 @@
 using namespace std;
 
 
-int longestDivisibleSubarray(vector<int> &expenses, int friends){
-    unordered_map<int, int> firstOccurrence;
-    firstOccurrence[0] = -1;
+// int longestDivisibleSubarray(vector<int> &expenses, int friends){
+//     unordered_map<int, int> firstOccurrence;
+//     firstOccurrence[0] = -1;
 
-    long long cumulativeSum = 0;
-    int maxDays = 0;
+//     long long cumulativeSum = 0;
+//     int maxDays = 0;
 
-    for(int day=0; day<expenses.size(); day++){
-        cumulativeSum += expenses[day];
+//     for(int day=0; day<expenses.size(); day++){
+//         cumulativeSum += expenses[day];
 
-        int remainder = (int)((cumulativeSum % friends + friends) % friends);
+//         int remainder = (int)((cumulativeSum % friends + friends) % friends);
 
-        if(!firstOccurrence.count(remainder)){
-            firstOccurrence[remainder] = day;
-        }else{
-            int length = day - firstOccurrence[remainder];
-            maxDays = max(maxDays, length);
+//         if(!firstOccurrence.count(remainder)){
+//             firstOccurrence[remainder] = day;
+//         }else{
+//             int length = day - firstOccurrence[remainder];
+//             maxDays = max(maxDays, length);
+//         }
+//     }
+
+//     return maxDays;
+
+
+// }
+
+
+
+
+int longestSubarray(vector<int> &arr, int k){
+
+    int maxLength = 0;
+// Updated version - testing git commit
+
+    for(int start=0; start<arr.size(); start++){
+
+        int sum = 0;
+
+        for(int end=start; end<arr.size(); end++){
+            
+            sum = (sum + arr[end]) % k;
+
+            if(sum == 0){
+                int length = end - start + 1;
+                maxLength = max(maxLength, length);
+            }
+
+
         }
     }
 
-    return maxDays;
-
-
+    return maxLength;
 }
+
+
+
+
 
 
 
@@ -36,7 +68,7 @@ int main(){
     vector<int> expenses = {2, 7, 6, 1, 4, 5};
     int friends = 3;
 
-    int result = longestDivisibleSubarray(expenses, friends);
+    int result = longestSubarray(expenses, friends);
     cout << "Maximum number of days expenses can be equally divided among friends: "
          << result << endl;
     return 0;
