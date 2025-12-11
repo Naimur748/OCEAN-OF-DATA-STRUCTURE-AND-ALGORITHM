e6c9c09b222e98f70cdf18c40ed13b0118bcd776
Md. Naimur Rahman <naimur748@gmail.com>
2025-12-11 22:26:29 +0600

    NQ problem using Bactracking

--- Backtracking/TargetSumCombinations.cpp
+++ Backtracking/TargetSumCombinations.cpp
@@ -3,6 +3,69 @@
 #include <algorithm>
 using namespace std;
 
+vector<int> results;
+
+void backtrack(vector<int>& arr, int start, int reamining, vector<int>& current){
+
+    if(reamining == 0){
+        result.push_back(current);
+        return;
+    }
+
+    for(int i=start; i < (int)arr.size(); i++){
+
+        if(arr[i] > reamining) break;
+
+        current.push_back(arr[i]);
+
+        backtrack(arr, i, reamining - arr[i], current);
+
+        current.pop_back();
+    }
+}
+
+
+
+
+int main(){
+
+    vector<int> arr = {1, 2, 3};
+    int target = 5;
+
+    sort(arr.begin(), arr.end());
+
+    vector<int> current;
+    results.clear();
+
+    backtrack(arr, 0, target, current);
+
+     if (results.empty()) {
+        cout << "[]\n";
+    } else {
+        cout << "[";
+        for (size_t i = 0; i < results.size(); ++i) {
+            cout << "[";
+            for (size_t j = 0; j < results[i].size(); ++j) {
+                cout << results[i][j];
+                if (j + 1 < results[i].size()) cout << ", ";
+            }
+            cout << "]";
+            if (i + 1 < results.size()) cout << ", ";
+        }
+        cout << "]\n";
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+/*
+
 vector<vector<int>> results;
 
 void backtrack(const vector<int>& arr, int start, int remaining, vector<int>& current){
@@ -59,3 +122,5 @@ int main(){
     return 0;
 
 }
+
+*/
