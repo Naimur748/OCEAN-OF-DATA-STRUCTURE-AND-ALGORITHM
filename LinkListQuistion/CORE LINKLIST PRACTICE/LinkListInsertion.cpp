fb8e6477a54e2ef12187dd63005eb82e6be2ce00
Md. Naimur Rahman <naimur748@gmail.com>
2026-01-24 10:53:32 +0600

    Link list Insertion practice

---
+++ LinkListQuistion/CORE LINKLIST PRACTICE/LinkListInsertion.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = nullptr;
+    }
+};
+
+void insertAtEnd(Node*& head, int val){
+
+    Node* newNode = new Node(val);
+
+    if(head == nullptr){
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    while(temp->next != nullptr){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
+};
+
+void printList(Node* head){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NILL" << endl;
+};
+
+int main(){
+
+Node* head = nullptr;
+
+insertAtEnd(head, 10);
+insertAtEnd(head, 20);
+insertAtEnd(head, 30);
+insertAtEnd(head, 40);
+insertAtEnd(head, 50);
+
+printList(head);
+
+return 0;
+
+}
